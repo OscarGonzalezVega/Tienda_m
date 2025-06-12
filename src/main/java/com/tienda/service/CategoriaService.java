6952940f38691ccab78fbb5f1b8211e9feaac42f
OscarGonzalezVega <ogonzalez10383@ufide.ac.cr>
2025-06-11 20:50:37 -0600

    Semana 5 : CRUD

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -18,7 +18,27 @@ public class CategoriaService {
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activo) {
         var lista = categoriaRepository.findAll();
+        
+        //Se hace el filtro si sólo se desean las categorías activas...
+        if (activo){
+            lista.removeIf(e -> !e.isActivo());
+        }
         return lista;
+    }   
+    
+    @Transactional(readOnly=true)
+    public Categoria getCategorias(Categoria categoria) {
+        return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+    }   
+    @Transactional
+    public void save(Categoria categoria){
+        categoriaRepository.save(categoria);     
     }
+    
+    @Transactional
+    public void delete(Categoria categoria){
+        categoriaRepository.delete(categoria);     
+    }
+    
     
 }
