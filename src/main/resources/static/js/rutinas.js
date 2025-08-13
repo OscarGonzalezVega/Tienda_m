e4cdbc6697175ee6e283689b777eeff50336496a
OscarGonzalezVega <ogonzalez10383@ufide.ac.cr>
2025-08-13 00:07:34 -0600

    Semana 12 : Carrito

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,3 +1,14 @@
+function addCart (formulario){
+    var idProducto = formulario.elements[0].value;
+    var existencias = formulario.elements[1].value;
+    if (existencias>0){
+        var ruta = "/carrito/agregar/"+idProducto;
+        $("#resultBlock").load(ruta);
+    }else {
+        alert("No hay existencias...");
+    }
+}
+
 function muestraImagen(input){
     if(input.files && input.files[0]){
         var lector = new FileReader();
@@ -11,16 +22,16 @@ function muestraImagen(input){
 }
 
 //Para insertar información en el modal según el registro...
-document.addEventListener('DOMContentLoaded', function () {
-    const confirmModal = document.getElementById('confirmModal');
-    confirmModal.addEventListener('show.bs.modal', function (event) {
-        const button = event.relatedTarget;
-        document.getElementById('modalId').value = button.getAttribute('data-bs-id');
-        document.getElementById('modalDescripcion').textContent = button.getAttribute('data-bs-descripcion');
-    });
-});
-
-
-setTimeout(() => {
-        document.querySelectorAll('.toast').forEach(t => t.classList.remove('show'));
-    }, 3000);
+//document.addEventListener('DOMContentLoaded', function () {
+//    const confirmModal = document.getElementById('confirmModal');
+//    confirmModal.addEventListener('show.bs.modal', function (event) {
+//        const button = event.relatedTarget;
+//        document.getElementById('modalId').value = button.getAttribute('data-bs-id');
+//        document.getElementById('modalDescripcion').textContent = button.getAttribute('data-bs-descripcion');
+//    });
+//});
+//
+//
+//setTimeout(() => {
+//        document.querySelectorAll('.toast').forEach(t => t.classList.remove('show'));
+//    }, 3000);
